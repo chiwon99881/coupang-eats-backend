0c833ce7c9d8f5d89e8cef80def1164f2f5274b2
chiwon <chiwon99881@gmail.com>
2021-02-27 11:39:57 +0900

    #02 NestConfig

--- README.md
+++ README.md
@@ -3,3 +3,19 @@
 - #01 First init
 
   > NestJS, GraphQL, TypeORM(postgres)
+
+  ```bash
+  npm i @nestjs/graphql graphql-tools graphql apollo-server-express
+
+  npm install --save @nestjs/typeorm typeorm pg
+  ```
+
+- #02 NestConfig
+
+  ```bash
+  npm i --save @nestjs/config
+
+  npm install --save-dev cross-env
+
+  npm install --save joi
+  ```
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,37 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      ignoreEnvFile: process.env.NODE_ENV == 'prod',
+      envFilePath: process.env.NODE_ENV == 'dev' ? '.env.dev' : '.env.test',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'prod', 'test').required(),
+        DATABASE_HOST: Joi.string().required(),
+        DATABASE_PORT: Joi.string().required(),
+        DATABASE_USERNAME: Joi.string().required(),
+        DATABASE_NAME: Joi.string().required(),
+      }),
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      synchronize: process.env.NODE_ENV !== 'prod',
+      logging: process.env.NODE_ENV !== 'prod',
+      entities: [],
     }),
     RestaurantsModule,
   ],
